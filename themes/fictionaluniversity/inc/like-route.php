4e04a1691649f587184b482baf7021eb49694f56
jarutosurano <jarutosurano@gmail.com>
2024-10-23 09:40:39 -0400

    Updat ACF plugin files

---
+++ themes/fictionaluniversity/inc/like-route.php
@@ -0,0 +1,15 @@
+<?php
+
+add_action('rest_api_init', 'universityLikeRoutes');
+
+function universityLikeRoutes() {
+    register_rest_route('university/v1', 'manageLike', array(
+        'methods' => 'POST',
+        'callback' => 'createLike'
+    ));
+
+    register_rest_route('university/v1', 'manageLike', array(
+        'methods' => 'DELETE',
+        'callback' => 'deleteLike'
+    ));
+};
